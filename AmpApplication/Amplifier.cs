ac9d1a873693bd1d0b9f87378ef06af383dc430f
K8504 <K8504@users.noreply.github.com>
2017-02-08 21:05:09 +0200

    Add files via upload

---
+++ AmpApplication/Amplifier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmpApplication
+{
+    class Amplifier
+    {
+        // Max Volume
+        private readonly int maxVolume = 100;
+        // Min Volume
+        private readonly int minVolume = 0;
+
+        private int volume;
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= maxVolume && value >= minVolume)
+                {
+                    volume = value;
+                }
+                else
+                {
+                    Console.WriteLine("Value you set isn't in limited area. Choose another one.");
+                }
+            }
+        }
+    }
+}
---
+++ AmpApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmpApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Amplifier amplifier = new Amplifier();
+
+            do
+            {
+                amplifier.Volume = 1;
+                Console.WriteLine("Choose amplifier's volume from 0-100");
+                string feed = Console.ReadLine();
+                amplifier.Volume = int.Parse(feed);
+
+                Console.WriteLine("Amplifier's volume is now: " + amplifier.Volume);
+            } while (1 == 1);
+        }
+    }
+}
